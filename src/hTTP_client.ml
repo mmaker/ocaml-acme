52da85728702e5741d8019b2b6e0cf23c8275493
Romain Calascibetta <romain.calascibetta@gmail.com>
2021-07-10 13:43:07 +0200

    Add a better documentation about the required HTTP client implementation

--- src/hTTP_client.ml
+++ src/hTTP_client.ml
@@ -31,8 +31,6 @@ module type S = sig
     (** [add hdrs key value] adds a [key] and a [value] to an existing
         [hdrs] headers. *)
 
-    val to_string : t -> string
-
     val get : t -> string -> string option
     (** [get hdrs key] retrieves a [key] from the given [hdrs] headers. If the
         header is one of the set of headers defined to have list values, then
@@ -41,34 +39,52 @@ module type S = sig
         is returned and no concatenation is performed. *)
 
     val get_location : t -> Uri.t option
+    (** [get_location hdrs] is [get hdrs "location"]. *)
 
     val init_with : string -> string -> t
     (** [init_with key value] constructs a fresh map of HTTP headers with a
         single key and value entry. *)
+
+    (** / *)
+
+    val to_string : t -> string
   end
 
   module Body : sig
     type t
+    (** The type of HTTP body. *)
 
     val of_string : string -> t
+    (** [of_string str] makes a body from the given [string] [str]. *)
+
     val to_string : t -> string Lwt.t
+    (** [to_string body] returns the full given [body] as a [string]. *)
   end
 
   module Response : sig
     type t
+    (** The type of HTTP response. *)
 
     val status : t -> int
+    (** [status resp] is the HTTP status code of the response [resp]. *)
+
     val headers : t -> Headers.t
+    (** [headers resp] is headers of the response [resp]. *)
   end
 
   val head :
     ?ctx:ctx -> ?headers:Headers.t -> Uri.t -> Response.t Lwt.t
+  (** [head ?ctx ?headers uri] sends an {i HEAD} HTTP request to the given
+      [uri] and returns its response. The returned response does not have
+      a {i body} according to the HTTP standard. *)
 
   val get :
     ?ctx:ctx ->
     ?headers:Headers.t ->
     Uri.t ->
     (Response.t * Body.t) Lwt.t
+  (** [get ?ctx ?headers uri] sends an {i GET} HTTP request to the given
+      [uri] and returns its response with its body. *)
 
   val post :
     ?ctx:ctx ->
@@ -77,4 +93,7 @@ module type S = sig
     ?headers:Headers.t ->
     Uri.t ->
     (Response.t * Body.t) Lwt.t
+  (** [post ?ctx ?body ?chunked ?headers uri] sends an {i POST} HTTP request
+      with the optional given [body] using chunked encoding if [chunked] is
+      [true] (default to [false]). It returns a response and a body. *)
 end
